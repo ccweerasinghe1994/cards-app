7e21af102e4b85ad763dc6ccab92d8951fd92708
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-21 11:03:56 +0530

    finishing up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,228 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpg",
+      userName: "nature",
+      content: "I saw Neat Tree Today"
+    },
+    {
+      title: "Green Mountain",
+      imageUrl: "assets/mountian.jpg",
+      userName: "mountainLover",
+      content: "Here is a picture of a Green Mountain"
+    },
+    {
+      title: "Mountain Biker",
+      imageUrl: "assets/biker.jpg",
+      userName: "bikingwc",
+      content: "I did some biking today"
+    },
+    
+  ];
 }
